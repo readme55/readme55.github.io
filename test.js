8269985e60d547d3ae35cffb48b0b00d055e8e89
readme <readme55@protonmail.com>
2020-07-27 21:51:40 +0200

    no message

---
+++ test.js
@@ -0,0 +1,6 @@
+var clientOpts = {};
+clientOpts.network = 'evonet';
+clientOpts.wallet = {};
+clientOpts.wallet.mnemonic = 'velvet timber under input escape rich gauge final submit burst glow garage';
+
+const client = new Dash.Client(clientOpts);
